e266abce37d04992a0531b23dc8bc8372fb2e11e
Shawn <shawn@kafei.io>
2019-01-06 19:06:55 -0600

    Make packaging functional

--- LBRY 3D Viewer/Assets/Editor/LBRY/package.js
+++ LBRY 3D Viewer/Assets/Editor/LBRY/package.js
@@ -3,3 +3,7 @@ console.log('package.js running in: ' + process.cwd());
 const lbryFormat = require('./lbry-format~');
 
 console.log(lbryFormat);
+
+lbryFormat.packDirectory('./', {
+  fileName: 'package.lbry',
+});
